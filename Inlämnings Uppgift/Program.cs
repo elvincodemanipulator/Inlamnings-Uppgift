d59066ff4be9d868b9a35c807d9ce49a7bb751a7
elvinborginger <elvin.borginger@gmail.com>
2026-09-11 13:05:42 +0200

    Add project files.

---
+++ Inlämnings Uppgift/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Inlämnings_Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken är din favorit bok?");
+            string Bok = Console.ReadLine();
+            Console.WriteLine(Bok);
+        }
+    }
+}
